06acd2b2ae801c9a44c71a2f4cefc4aa117e492e
miyokonakada <fortissimo_pikkoro@yahoo.co.jp>
2025-02-03 23:29:31 +1000

    added delete feature 0203

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-# from .views import hello_world
 from . import views
-
-# urlpatterns = [
-#     path('hello/', hello_world),  # `http://localhost:8000/api/hello/` でアクセス可能
-# ]
 
 urlpatterns = [
     path('record-dinner/', views.record_dinner, name='record_dinner'),
     path('history/', views.history, name='history'),
+    path('delete-dinner-record/<int:record_id>/', views.delete_dinner_record, name='delete'),
+    # path('get_csrf_token/', views.get_csrf_token, name='get_csrf_token'),
+    
 ]
