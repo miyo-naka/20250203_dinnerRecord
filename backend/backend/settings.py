09bb8d56dfb6e9590ea2d5219d1acbed0ef70452
miyokonakada <fortissimo_pikkoro@yahoo.co.jp>
2025-02-10 15:14:04 +1000

    changed settings.py for deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,10 +133,12 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://miyo.pythonanywhere.com"
+    "http://miyo.pythonanywhere.com",
+    "https://miyo-naka.github.io/"
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://miyo.pythonanywhere.com"
+    "http://miyo.pythonanywhere.com",
+    "https://miyo-naka.github.io/"
 ]
